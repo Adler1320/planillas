7bb970724743fe5527f50f3534a35d4a4ab01561
Adler Ramos <ramos31627764@gmail.com>
2024-06-12 10:33:20 -0600

    Inicio

---
+++ navegacion/slidebaradmin.php
@@ -0,0 +1,198 @@
+<!-- Sidebar -->
+<div class="sidebar" data-background-color="dark">
+    <div class="sidebar-logo">
+        <!-- Logo Header -->
+        <div class="logo-header" data-background-color="dark">
+            <a href="index.php" class="logo">
+                <img src="assets/img/kaiadmin/logo_light.svg" alt="navbar brand" class="navbar-brand" height="20" />
+            </a>
+            <div class="nav-toggle">
+                <button class="btn btn-toggle toggle-sidebar">
+                    <i class="gg-menu-right"></i>
+                </button>
+                <button class="btn btn-toggle sidenav-toggler">
+                    <i class="gg-menu-left"></i>
+                </button>
+            </div>
+            <button class="topbar-toggler more">
+                <i class="gg-more-vertical-alt"></i>
+            </button>
+        </div>
+        <!-- End Logo Header -->
+    </div>
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+            <ul class="nav nav-secondary">
+                <li class="nav-item">
+                    <a href="index.php">
+                        <i class="fas fa-home"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#">
+                        <i class="fas fa-store"></i>
+                        <p>Empresas</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#">
+                        <i class="fas fa-comment-alt"></i>
+                        <p>Notificaciones</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#">
+                        <i class="fas fa-envelope"></i>
+                        <p>Mensajes</p>
+                    </a>
+                </li>
+
+
+                <li class="nav-section">
+                    <span class="sidebar-mini-icon">
+                        <i class="fa fa-ellipsis-h"></i>
+                    </span>
+                    <h4 class="text-section">MENU</h4>
+                </li>
+
+                <li class="nav-item">
+                    <a data-bs-toggle="collapse" href="#base">
+                        <i class="fas fa-user"></i>
+                        <p>Empleados</p>
+                        <span class="caret"></span>
+                    </a>
+                    <div class="collapse" id="base">
+                        <ul class="nav nav-collapse">
+                            <li>
+                                <a href="#">
+                                    <span class="sub-item">Ficha de Ingreso</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span class="sub-item">Liquidaciones</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span class="sub-item">Asignaciones y Traslados</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a href="#">
+                        <i class="fas fa-calendar-minus"></i>
+                        <p>Planillas</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#">
+                        <i class="fas fa-file-invoice-dollar"></i>
+                        <p>Descuentos</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#">
+                        <i class="fas fa-piggy-bank"></i>
+                        <p>Ahorros</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#">
+                        <i class="fas fa-money-bill"></i>
+                        <p>Prestamos</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#">
+                        <i class="fas fa-bolt"></i>
+                        <p>Anticipos</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#">
+                        <i class="fas fa-umbrella-beach"></i>
+                        <p>Vacaciones</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#">
+                        <i class="fas fa-user-slash"></i>
+                        <p>Suspenciones</p>
+                    </a>
+                </li>
+
+                <li class="nav-section">
+                    <span class="sidebar-mini-icon">
+                        <i class="fas fa-user-alt-slash"></i>
+                    </span>
+                    <h4 class="text-section">Ajustes</h4>
+                </li>
+
+                <li class="nav-item">
+                    <a data-bs-toggle="collapse" href="#org">
+                        <i class="fas fa-server"></i>
+                        <p>Organización</p>
+                        <span class="caret"></span>
+                    </a>
+                    <div class="collapse" id="org">
+                        <ul class="nav nav-collapse">
+                            <li>
+                                <a href="#">
+                                    <span class="sub-item">Usuarios</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span class="sub-item">Puestos</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a data-bs-toggle="collapse" href="#config">
+                        <i class="fas fa-user-cog"></i>
+                        <p>Configuración</p>
+                        <span class="caret"></span>
+                    </a>
+                    <div class="collapse" id="config">
+                        <ul class="nav nav-collapse">
+                            <li>
+                                <a href="#">
+                                    <span class="sub-item">Descuentos y Bonos</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span class="sub-item">Intereses</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a href="#">
+                        <i class="fas fa-user-circle"></i>
+                        <p>Mi Perfil</p>
+                    </a>
+                </li>
+                
+
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- End Sidebar -->
