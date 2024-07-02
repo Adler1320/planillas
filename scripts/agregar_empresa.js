7238d82a8f84ccfa46687eafd29ac16e0c781f1a
AdlerR1320 <ramos31627764@gmail.com>
2024-07-01 21:19:17 -0600

    s

--- scripts/agregar_empresa.js
+++ scripts/agregar_empresa.js
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         // Enviar formulario con AJAX
-        fetch('http://localhost/planillas/tablas/insertarlocales.php', {
+        fetch('http://localhost:8080/planillas/tablas/insertarlocales.php', {
             method: 'POST',
             body: formData
         })
